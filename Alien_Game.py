c5ab9a6a8b9176b2b5a6f10823fec1a809dfea1b
Egor Melnikov <pc.player@mail.ru>
2024-08-07 22:28:08 +0300

    ship_dont_work

--- Alien_Game.py
+++ Alien_Game.py
@@ -1,19 +1,18 @@
 import pygame
 import sys
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-
-
-
         self.screen = pygame.display.set_mode((self.settings.screen_width, 
                                                self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.bg_color = (230, 230, 230)
-
+        self.ship = Ship(screen)
+        
     def run_game(self):
         while True:
             for event in pygame.event.get():
@@ -21,9 +20,10 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             pygame.display.flip()
 
 if __name__ == "__main__":
-    game = AlienInvasion()
-    game.run_game()
+    ai = AlienInvasion()
+    ai.run_game()
