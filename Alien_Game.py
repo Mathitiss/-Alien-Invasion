88a15d1a0c09edd41edf557bb53ad4483e80fe06
Egor Melnikov <pc.player@mail.ru>
2024-08-10 22:13:18 +0300

    defs_add

--- Alien_Game.py
+++ Alien_Game.py
@@ -10,19 +10,23 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, 
                                                self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230, 230, 230)
         self.ship = Ship(self)
         
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            pygame.display.flip()
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     ai = AlienInvasion()
