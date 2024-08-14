71d7687fd054d3a662d221ddd45026b64b5720ab
Egor Melnikov <pc.player@mail.ru>
2024-08-14 20:11:29 +0300

    bullet

--- Alien_Game.py
+++ Alien_Game.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     def __init__(self):
@@ -17,11 +18,13 @@ class AlienInvasion:
                                                self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         
     def run_game(self):
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -45,6 +48,9 @@ class AlienInvasion:
         if event.key == pygame.K_DOWN or event.key == pygame.K_s:
             self.ship.moving_down = True
 
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
         elif event.key == pygame.K_ESCAPE:
             sys.exit()
 
@@ -59,10 +65,16 @@ class AlienInvasion:
         if event.key == pygame.K_DOWN or event.key == pygame.K_s:
             self.ship.moving_down = False
 
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
 
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 if __name__ == "__main__":
